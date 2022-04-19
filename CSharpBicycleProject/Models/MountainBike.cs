77fb0c8206dc786916c3905e1d67622a2ad5a53a
Colin Herbst <colinherbst15@gmail.com>
2022-04-19 15:57:45 -0500

    CAC-103 Guided Project child classes, Serial and options

---
+++ CSharpBicycleProject/Models/MountainBike.cs
@@ -0,0 +1,9 @@
+﻿namespace CSharpBicycleProject;
+
+internal abstract class MountainBike : Bicycle
+{
+    public override string Style { get; } = "MountainBike";
+    
+    protected MountainBike(IWheelInterface wheel)
+        : base(wheel) { }
+} // end class
